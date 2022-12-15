e8b27bc51863b0b0586d535e784497cef91b9fd4
dalm <dalm@gmv.com>
2022-12-15 01:33:51 +0100

    hooola!

--- test_transformers.py
+++ test_transformers.py
@@ -10,9 +10,7 @@ from utils.model import AudioClassifier, ModelDimensions
 from utils.dataset import NUM_CLASSES, get_dataset, SongDataset_v2, SongDatasetTest_v2
 from utils.train import train, valid_epoch, calculate_confusion_matrix, plot_confusion_matrix
 
-def main(l_rate, gamma, n_epochs, batch_size, save_path, seed):
-    # replicability
-    torch.manual_seed(seed)
+def main(batch_size, save_path):
 
     # select device 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -21,35 +19,18 @@ def main(l_rate, gamma, n_epochs, batch_size, save_path, seed):
     loss_fn = torch.nn.CrossEntropyLoss()
 
     # load dataset
-    dataset = get_dataset('annotations_v2.csv')
-    train_set, valid_set, test_set = random_split(dataset, [0.8,0.1,0.1], generator=torch.Generator().manual_seed(seed))
+    dataset = get_dataset('annotations_test.csv')
+    #train_set, valid_set, test_set = random_split(dataset, [0.8,0.1,0.1], generator=torch.Generator().manual_seed(seed))
 
-    # load train & validation loader
-    train_set = SongDataset_v2(train_set)
-    valid_set = SongDatasetTest_v2(valid_set)
-
-    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=4, shuffle=True)
-    valid_loader = DataLoader(valid_set, batch_size=batch_size, num_workers=4)
-    
     # select model
     dims = ModelDimensions(n_mels=80, n_audio_ctx=1500, n_audio_state=384, n_audio_head=6, n_audio_layer=4)
     
-    model = AudioClassifier(dims).to(device)
-    # model.load_state_dict(torch.load('models/pretrained.pth'))
-
-    # logger
-    log_path = os.path.join('log', datetime.now().strftime("%Y_%m_%d-%H_%M_%S"))
-    writer = SummaryWriter(log_path)
-
-    # train
-    train(model, train_loader, valid_loader, loss_fn, device, save_path, writer, lr=l_rate, n_epochs=n_epochs, gamma=gamma)
-
     # load best model
     model = AudioClassifier(dims).to(device)
     model.load_state_dict(torch.load(save_path))
 
     # test
-    test_set = SongDatasetTest_v2(test_set)
+    test_set = SongDatasetTest_v2(dataset)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4)
     test_loss, test_accuracy = valid_epoch(model, test_loader, loss_fn, device)
     print(f"Test loss: {test_loss}; Test accuracy: {test_accuracy}")
@@ -61,12 +42,8 @@ def main(l_rate, gamma, n_epochs, batch_size, save_path, seed):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--l_rate', type=float, default=3e-5, help='learning rate')
-    parser.add_argument('--gamma', type=float, default=0.95, help='gamma parameter for optimizer scheduler')   
-    parser.add_argument('--n_epochs', type=int, default=50, help='number of epochs')
     parser.add_argument('--batch_size', type=int, default=4, help='batch size')
     parser.add_argument('--save_path', type=str, default='models/best_model_transformer.pth', help='best model saved path')
-    parser.add_argument('--seed', type=int, default=1234)
     kwargs = parser.parse_args()
 
     main(**vars(kwargs))
